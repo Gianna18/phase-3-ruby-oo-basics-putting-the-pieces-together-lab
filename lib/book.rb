07f814cacd027b2820d567bfbfb394f7331d57ce
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-02-11 13:47:50 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+#defining book class and using Macros 
+class Book 
+    attr_accessor :title, :author, :page_count, :genre 
 
+    def initialize(title)
+        @title=title  
+
+    end 
+    def turn_page
+        p "Flipping the page...wow, you read fast!"
+    end 
+
+    end 
 
